c43fce9d1d49d0a4664c58ddf0b2789d3f4b79d7
jchanme <39323414+jchanme@users.noreply.github.com>
2018-05-28 15:22:23 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Report Customize",
+    'summary': """Customize Report""",
+    'description': """Customize Report""",
+    'author': "Safyric Co., Ltd.",
+    'website': "https://www.safyric.com",
+    'category': 'Business',
+    'version': '0.1',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'reports/external_layout_background.xml',
+    ],
+    'installable': True,
+}
